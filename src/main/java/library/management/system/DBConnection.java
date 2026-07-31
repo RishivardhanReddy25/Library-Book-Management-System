f0d367217673758037f25f70ad94256b13cd4049
Rishi Vardhan Reddy <rishivardhanreddy25@gmail.com>
2026-07-31 20:00:38 +0530

    Update database connection for cloud host

--- src/main/java/library/management/system/DBConnection.java
+++ src/main/java/library/management/system/DBConnection.java
@@ -12,7 +12,11 @@ public class DBConnection {
     private static final String USER = "root";
 
     private static final String PASSWORD = "12345";
-
+    
+    private static final  String url = "jdbc:mysql://your-cloud-db-host.com:3306/your_db_name?useSSL=true";
+    private static final  String user = "root";
+    private static final  String password = "dAdUQEvcArjjquZBsrioVEWpGoZEcSUN";
+    
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
